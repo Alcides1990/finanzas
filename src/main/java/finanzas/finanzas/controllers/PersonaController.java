45e96616a8698b27f0427a6b966ca43025cdbd1a
Alcides Noguera <alcidesnoguera1990@gmail.com>
2022-11-21 22:28:58 -0400

    xml response

--- src/main/java/finanzas/finanzas/controllers/PersonaController.java
+++ src/main/java/finanzas/finanzas/controllers/PersonaController.java
@@ -7,6 +7,7 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import finanzas.finanzas.models.PersonaModel;
@@ -27,7 +29,7 @@ public class PersonaController {
     @Autowired
     private IPersonaRepository repo;
 
-    @GetMapping("findAll")
+    @RequestMapping( value = "/findAll",method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<List<PersonaModel>> findAll(){
         List<PersonaModel> datos= repo.findAll();
         try{
